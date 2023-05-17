a6dfc9e3017f9527f0dc00ca2ddeb017f3d2cc72
don't have any <vlad.luckashonok@ya.ru>
2023-05-17 09:14:46 +0300

    Rebuild project with SignalR

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Server
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string username, string message)
+        {
+            await Clients.All.SendAsync("Receive", username, message);
+        }
+    }
+}
