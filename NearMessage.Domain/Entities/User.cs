18180a4fb7ab783ff15161c64e3c63d6e8da2cd6
don't have any <vlad.luckashonok@ya.ru>
2023-05-17 22:34:31 +0300

    Create User object domain layer

---
+++ NearMessage.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace NearMessage.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Password { get; set; }
+}
